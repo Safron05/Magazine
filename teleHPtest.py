8ed521c670248c1df51cedc67131efa753fe1ced
Safron05 <81923509+Safron05@users.noreply.github.com>
2021-04-11 23:17:35 +0300

    Добавили выключалку2

--- teleHPtest.py
+++ teleHPtest.py
@@ -91,5 +91,6 @@ else:
         bot.remove_webhook()
         bot.set_webhook(url='https://limitless-thicket-26129.herokuapp.com/' + TOKEN)
         return "!", 200
+    webhook
     if __name__ == "__main__":
         server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
